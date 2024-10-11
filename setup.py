fd7341b7f56b0c8162ce70df206d40833cb63eee
plumiume <plumiume@gmail.com>
2024-10-11 19:45:13 +0900

    First Commit (2)

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='conformer_utils',
     version='0.0.0',
-    packages=find_packages(),
+    packages=[
+        'conformer_utils'
+    ],
     install_requires=[
         'torch',
         'torchaudio'
